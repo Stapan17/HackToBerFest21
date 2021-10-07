faf6699aaabbe8d9c88adf098bd8cb0dcee91906
Hathaipach <hathaipach@hotmail.com>
2021-10-07 20:27:49 +0700

    Create unit12_assertNotIsInstance.py
    Method assertNotIsInstance

---
+++ unit12_assertNotIsInstance.py
@@ -0,0 +1,21 @@
+import unittest
+
+# Method assertNotIsInstance()
+# UnitTestBy Hathaipach I.
+
+class Empclass:
+    Name = 'Maria'
+    Birthday = '10-Dec-1997'
+    Garden = 'Male'
+
+class TestClass(unittest.TestCase):
+    # test function to test whether obj is instance of class
+    def test_positive(self):
+        objectName = 'Bangkok'
+        # error message in case if test case got failed
+        message = "An object is instance of Empclass."
+        self.assertNotIsInstance(objectName,Empclass,message)
+
+
+if __name__ == '__main__':
+    unittest.main()
